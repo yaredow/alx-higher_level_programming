02cdc340678576e3eb315681b952bba522f66563
yaredow <yaredyilma11@gmail.com>
2021-11-16 10:43:45 -0800

    added str format

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("{}".format("Fizzbuzz", end=""))
+        elif number % 3 == 0:
+            print("{}".format("Buzz", end=""))
+        elif number % 5 == 0:
+            print("{}".format("Fizz", end=""))
+        else:
+            print({}.format(number), end="")
