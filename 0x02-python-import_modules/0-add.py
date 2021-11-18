8686dd23261f1559d95bf5d50b468c97756c33c9
yaredow <yaredyilma11@gmail.com>
2021-11-18 09:30:45 -0800

    added docstring

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-from add_0 import add
 if __name__ == "__main__":
-    """Prints the sun of a and b"""
+    """Print the sum of 1 and 2."""
+    from add_0 import add
 
-a = 1
-b = 2
-print("{} + {} = {}".title(a, b, add(a, b)))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
