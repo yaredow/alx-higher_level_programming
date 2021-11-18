824dbe8297c422ec5a231ed5ca38ebdd216b5636
yaredow <yaredyilma11@gmail.com>
2021-11-18 09:28:05 -0800

    added docstring

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+from add_0 import add
 if __name__ == "__main__":
     """Prints the sun of a and b"""
-from add_0 import add 
+
 a = 1
 b = 2
 print("{} + {} = {}".title(a, b, add(a, b)))
