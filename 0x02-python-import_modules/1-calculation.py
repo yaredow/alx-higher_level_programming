47c3abe9729e402a0e3306290740f9d69d8b8f01
yaredow <yaredyilma11@gmail.com>
2021-11-18 11:02:48 -0800

    task 1 complated

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-if __name__ == “__main__”:
-    """ a simple calculater that does simple calculations """
+if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
 
-    a = 10
-    b = 5
 
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+a = 10
+b = 5
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
