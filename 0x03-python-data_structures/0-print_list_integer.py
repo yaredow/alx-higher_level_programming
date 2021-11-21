4100049b59853f4c39af33ae65b90123af3c6df2
yaredow <yaredyilma11@gmail.com>
2021-11-21 08:11:04 -0800

    task 0 complated

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -4,4 +4,4 @@
 def print_list_integer(my_list=[]):
     """ prints integer of a list """
     for i in range(len(my_list)):
-        print("{:d}".format(i))
+        print("{:d}".format(my_list[i]))
