ede5b145b67355f78357ec732ebf9b92552daddb
yaredow <yaredyilma11@gmail.com>
2021-11-22 01:34:58 -0800

    task 21 complated

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,8 @@
 
 
 def element_at(my_list, idx):
-        """ retrieves an element from a list like in C """
-        if idx < 0 or idx > (len(my_list) - 1):
-            return None
-        else:
-            return (my_list[idx])
+    """ retrieves an element from a list like in C """
+    if idx < 0 or idx > (len(my_list) - 1):
+        return None
+    else:
+        return (my_list[idx])
