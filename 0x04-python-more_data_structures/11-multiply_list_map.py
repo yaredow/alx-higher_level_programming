c64d533d90632e842a07e580ee04e026c39ded70
yaredow <yaredyilma11@gmail.com>
2021-11-25 08:04:52 -0800

    task 13 complated

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 
-def delete_at(my_list=[], idx=0):
-    """Delete an item at a specific position in a list."""
-    if idx >= 0 and idx < len(my_list):
-        del my_list[idx]
-    return (my_list)
+def mutiply_list_map(my_list=[], number=0):
+    return list(map(lambda v: v * number, my_list))
