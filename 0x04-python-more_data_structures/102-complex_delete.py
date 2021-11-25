dd56f9a77145bb3fab74eba3f489b55f3bbfe336
yaredow <yaredyilma11@gmail.com>
2021-11-25 08:08:27 -0800

    task 12 complated

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-
-
-def complex_delete(a_dictionary, value):
-    for item in list(a_dictionary.keys()):
-        if a_dictionary[item] is value:
-            del a_dictionary[item]
-    return a_dictionary
+a = 89
+b = 10
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))
