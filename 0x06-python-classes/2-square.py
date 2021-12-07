fce25c696f96e58635871b7a086c89e5a8509fb8
yaredow <yaredyilma11@gmail.com>
2021-12-07 11:06:34 -0800

    task 2 complated

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,15 +2,19 @@
 """My square module"""
 
 
-def __init__(self, size=0):
-    """Define a square"""
-    self.__size = size
-try:
-    type(size) = int
-except TypeError:
-    print("size must be an integer")
-try:
-    if size < 0:
-except ValueError:
-    print("size must be >= 0")
-        
+class Square:
+    """defines a square"""
+
+    def __init__(self, size=0):
+        """Create a Square
+        Args:
+            size: length of side
+        Raises:
+            TypeError: if size != int
+            ValueError: if size < 0
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size        
