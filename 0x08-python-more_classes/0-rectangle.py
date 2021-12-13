cd54c14357de708e6d0322d6de1568121e441e47
yaredow <yaredyilma11@gmail.com>
2021-12-13 10:50:48 +0300

    added docstring

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 class Rectangle:
+    """My rectangle module"""
     pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+
