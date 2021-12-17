4264e9fe208839f0d5cd3ade276b66c91afec676
yaredow <yaredyilma11@gmail.com>
2021-12-17 18:24:32 +0300

    deleted all the files

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
-"""
-This module prints the name of a anything
-Output: 'My name is <first name> <last name>'
-    * The first name and last name must be a strings
-"""
+# 3-say_my_name.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines a name-printing function."""
 
 
 def say_my_name(first_name, last_name=""):
-    """
-    Prints the names
+    """Print a name.
+
     Args:
-        first_name (str): The first name of anything.
-        last_name (str): The last name of anything.
+        first_name (str): The first name to print.
+        last_name (str): The last name to print.
     Raises:
-        TypeError: If `first_name` and `last_name` aren't strings.
+        TypeError: If either of first_name or last_name are not strings.
     """
-
-    if type(first_name) is not str:
-        raise TypeError('first_name must be a string')
-
-    if type(last_name) is not str:
-        raise TypeError('last_name must be a string')
-
-    print('My name is', first_name, last_name)
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,34 +1,23 @@
 #!/usr/bin/python3
-"""
-This module prints a square with the character '#'
-of any positive integer size.
-Example:
-    ###
-    ###
-    ###
-    * The size must be an integer greater than 0.
-"""
+# 4-print_square.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines a square-printing function."""
 
 
 def print_square(size):
-    """
-    Prints the names
+    """Print a square with the # character.
+
     Args:
-        size (int): The size of the square to prints.
+        size (int): The height/width of the square.
     Raises:
-        TypeError: If `size` isn't integer.
-        ValueError: If `size` is less than 0.
+        TypeError: If size is not an integer.
+        ValueError: If size is < 0
     """
-
-    if type(size) is not int:
-        raise TypeError('size must be an integer')
-
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
     if size < 0:
-        raise ValueError('size must be >= 0')
+        raise ValueError("size must be >= 0")
 
-    for i in range(1, size + 1):
-        for j in range(1, size + 1):
-            print('#', end='')
-
-            if j % size == 0 and j > 0:
-                print()
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
+        print("")
