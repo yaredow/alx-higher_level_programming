6bfe35442c100249337245afc64fce5110e5fc75
yaredow <yaredyilma11@gmail.com>
2021-12-21 22:11:57 +0300

    task 5 completed

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""to_json string module"""
+"""module for from_json_string"""
 import json
 
 
-def to_json_string(my_obj):
-    """returns the JSON representation of an object (string)"""
-    return json.dumps(my_obj)
+def from_json_string(my_str):
+    """returns an object (Python data structure) represented by a JSON"""
+    return json.loads(my_str)
