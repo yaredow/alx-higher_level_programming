b69c37f72edd97dd422c30c37d559531fd98ae1e
yaredow <yaredyilma11@gmail.com>
2021-12-21 21:26:46 +0300

    added len function and its value will be returned

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -9,6 +9,4 @@ def number_of_lines(filename=""):
     """
     count = 0
     with open(filename, encoding='utf-8') as file:
-        for line in file:
-            count += 1
-        return count
+        return file.write("text")
