548b96ab7266d10d70f8d524c29607a5eb64ffa0
yaredow <yaredyilma11@gmail.com>
2021-12-21 18:44:23 +0300

    modified the documentation of file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
+#!/bin/user/python3
+
+
 def read_file(filename=""):
-"""My read file module"""
+    """My read file module"""
     with open('filename=""') as f:
-        """Read file from a text"""
         file = f.read()
         print(file)
