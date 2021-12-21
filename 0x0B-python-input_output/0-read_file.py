d05d2cc2644d7a009be7eaff862c97a51ac62af8
yaredow <yaredyilma11@gmail.com>
2021-12-21 18:58:58 +0300

    removed the double quotation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """Read a text file"""
-    with open("filename, encoding='utf-8'") as f:
+    with open(filename, encoding='utf-8') as f:
         file = f.read()
         print(file, end="")
