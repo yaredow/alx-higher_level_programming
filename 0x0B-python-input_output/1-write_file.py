176b2fd25e32986eb4fd43fad30d0768c082c3c1
yaredow <yaredyilma11@gmail.com>
2021-12-21 21:53:58 +0300

    task 2 completed

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,5 +3,5 @@
 
 
 def write_file(filename="", text=""):
-    with open(filename, "w", encoding='UTF-8') as file:
+    with open(filename, "w+", encoding='UTF-8') as file:
         return file.write(text)
