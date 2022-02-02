dabda09c565970e13971391907302342f5861a7f
yaredow <yaredyilma11@gmail.com>
2022-02-02 08:22:39 -0800

    added all the rows

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,13 +1,13 @@
--- 9-full_creation.sql 
--- creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
-
+-- 9-full_creation.sql
+-- creates a table second_table, add rows
 CREATE TABLE IF NOT EXISTS second_table (
-	id INT, 
-	name, VARCHAR(256),
+	id INT,
+	name VARCHAR(256),
 	score INT
-);	
-INSERT INTO second_table 
+				
+) ENGINE=INNODB;
+INSERT INTO second_table
 VALUES (1, "John", 10),
-       (2, "Alex", 3),
-       (3, "Bob", 14),
-       (4, "George", 8);
+	(2, "Alex", 3),
+	(3, "Bob", 14),
+	(4, "George", 8);
