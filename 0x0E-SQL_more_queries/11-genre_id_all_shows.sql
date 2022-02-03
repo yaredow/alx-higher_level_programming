287e495a8685e4e1c589befe998133826a188fd4
yaredow <yaredyilma11@gmail.com>
2022-02-03 09:08:20 -0800

    task 101 complated

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,7 @@
 -- 100-not_my_genres.sql
 --  uses the hbtn_0d_tvshows database to list all genres not linked to the show
 --  Dexter
+
 SELECT DISTINCT tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
