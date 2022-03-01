58a8a82e9f318363fa1a4bff72e26b5a73bfd63f
yaredow <yaredyilma11@gmail.com>
2022-03-01 05:13:06 -0800

    task 2 complated

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
-const count = process.argv.length;
-console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
+if (process.argv.length <= 2) {
+  console.log('No argument');
+} else {
+  console.log((process.argv.length > 3 ? 'Arguments' : 'Argument') + ' found');
+}
