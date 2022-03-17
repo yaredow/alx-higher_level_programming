68431072c70c57c5c8b723f8f705b23d507509e5
yaredow <yaredyilma11@gmail.com>
2022-03-17 11:40:43 -0700

    task 5 complated

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a GET request to the URL, and displays the body of the response
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+# Send a GET request to a given URL with a header variable.
+curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
