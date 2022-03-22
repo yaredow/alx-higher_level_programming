9ca14e7654e86a09cf05cc159764305050b09b33
yaredow <yaredyilma11@gmail.com>
2022-03-22 04:35:48 -0700

    task 0 complated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""script that fetches 'https://alx-intranet.hbtn.io/status' """
-if __name__ == "__main__":
-    import urllib.request
+""" module that fetches https://intranet.hbtn.io/status"""
+import urllib.request as request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') \
-            as response:
-        html = response.read()
-    print("Body response:")
-    print('\t- type: {}'.format(type(html)))
-    print('\t- content: {}'.format(html))
-    print('\t- utf8 content: {}'.format(html.decode('UTF8')))
+
+if __name__ == "__main__":
+    with request.urlopen("https://intranet.hbtn.io/status") as result:
+        body = result.read()
+        print("Body response:\n\t- type: {}\n\t- content: {}\n\t\
+- utf8 content: {}".format(type(body), body, body.decode("utf-8")))
